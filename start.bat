f99ad61c3a50889b034a07f8c1b246e0319e5965
F4ir <109984628+F4ir@users.noreply.github.com>
2024-07-06 17:13:30 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@
 :: Get the directory of the batch file
 set "batchDir=%~dp0"
 
-:: Open the first Command Prompt and run pocketbase.exe serve in the pocketbase folder
+:: Open the first Command Prompt and run ds.py in the folder
 start cmd /k "cd /d %batchDir% && py ds.py"
