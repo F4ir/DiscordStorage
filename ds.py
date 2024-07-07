841c0dc897201ec64b119a50e2bbf944a6a8af7f
F4ir <109984628+F4ir@users.noreply.github.com>
2024-07-07 18:05:10 +0200

    Update ds.py

--- ds.py
+++ ds.py
@@ -81,7 +81,7 @@ def share_file(file_path):
 def parse_args(inp):
     commands = ['-h', '-help', '-l', '-list', '-d', '-download', '-u', '-upload', '-delete', '-del', '-s', '-share']
     if len(inp) == 1:
-        print('----------------------\n| DiscordStorage v0.6 |\n----------------------')
+        print('----------------------\n| DiscordStorage v0.7 |\n----------------------')
         print('| github.com/F4ir     |\n----------------------')
         print('\nUsage: python ds.py [command] (target)\n')
         print('COMMANDS:')
